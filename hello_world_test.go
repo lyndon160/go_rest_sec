474a4d3458c0d18499d7f5dfd9e885f4f5a946f8
Lyndon Fawcett <lyndonfawcett@hotmail.com>
2019-03-14 15:41:58 +0000

    Cleaned up test cases

--- hello_world_test.go
+++ hello_world_test.go
@@ -4,22 +4,24 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	}
+
 	t.Run("Saying hello to people", func(t *testing.T) {
 		got := Hello("Lyndon")
 		want := "Hello, Lyndon"
+		assertCorrectMessage(t, got, want)
 
-		if got != want {
-			t.Errorf("Got '%s' want '%s'", got, want)
-		}
 	})
 
 	t.Run("Say 'Hello, world.' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, world."
-
-		if got != want {
-			t.Errorf("Got '%s' want '%s'", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 }
